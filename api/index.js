1618112d1d8ade7eefc843fcca215f29fcfae9d6
Fenil05 <fenilgajjar2265@gmail.com>
2024-03-23 19:03:21 +0530

    Complete signup functionality

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import cors from "cors"
 import userRoute from "./routes/user.route.js"
 import authRoute from "./routes/auth.route.js"
 
 const app = express()
 dotenv.config()
 app.use(express.json())
+
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
+
 
 const connect = async()=>{
     try {
