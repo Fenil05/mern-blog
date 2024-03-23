65a098c9f4df6319180cd208b321a93a46072730
Fenil05 <fenilgajjar2265@gmail.com>
2024-03-23 17:44:00 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 
 const app = express()
+dotenv.config()
+
+const connect = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("Connected to MongoDB!");
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 app.listen(3000,()=>{
+    connect()
     console.log("Backend server is running!");
 })
